a7cbc046003f0ec8240383d6326b4d54138836c9
Vivek Vijayan <vivekvijayan6282@gmail.com>
2024-01-27 14:03:00 +0530

    sound

--- app.py
+++ app.py
@@ -3,67 +3,52 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-import time
+import sounddevice as sd
+import threading
 
-vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+from handtracking import draw_landmarks
 
-MARGIN = 10
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)
+frequency = 440.0
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
+def synth():
+    fs = 44100
 
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
+    while True:
+        print('hi')
+        wave = np.sin(2 * np.pi * frequency * np.arange(fs) / fs)
 
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend([
-        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
-        ])
-        solutions.drawing_utils.draw_landmarks(
-        annotated_image,
-        hand_landmarks_proto,
-        solutions.hands.HAND_CONNECTIONS,
-        solutions.drawing_styles.get_default_hand_landmarks_style(),
-        solutions.drawing_styles.get_default_hand_connections_style())
+        sd.play(wave, fs)
+        sd.wait()
 
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
+def webcam():
+    vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-        cv2.putText(annotated_image, f"{handedness[0].category_name}",
-                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
-                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+    while True:
+        ret, frame = vid.read()
 
-    return annotated_image
+        base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+        options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
+        detector = vision.HandLandmarker.create_from_options(options)
 
-while True:
-    ret, frame = vid.read()
+        image =  mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.flip(frame, 1))
+        detection_result = detector.detect(image)
 
-    base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-    options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-    detector = vision.HandLandmarker.create_from_options(options)
+        annotated_image = draw_landmarks(image.numpy_view(), detection_result)
+        cv2.imshow('', annotated_image)
 
-    image =  mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.flip(frame, 1))
-    detection_result = detector.detect(image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    print(detection_result.hand_landmarks[1])
+    vid.release() 
+    
+    cv2.destroyAllWindows()
 
-    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-    cv2.imshow('', annotated_image)
+if __name__ == "__main__":
+    synth_thread = threading.Thread(target=synth)
+    webcam_thread = threading.Thread(target=webcam)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    synth_thread.start()
+    webcam_thread.start()
 
-vid.release() 
- 
-cv2.destroyAllWindows()
+    synth_thread.join()
+    webcam_thread.join()
